301d41db4867977837a12b154c3b3ec814e686be
root <greenski@gmail.com>
2017-06-20 10:23:47 +0000

    compatibilité avec jeedom v3

--- desktop/php/fbbot.php
+++ desktop/php/fbbot.php
@@ -2,141 +2,151 @@
 if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
-sendVarToJS('eqType', 'fbbot');
-$eqLogics = eqLogic::byType('fbbot');
+$plugin = plugin::byId('fbbot');
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
-  <div class="col-lg-2 col-md-3 col-sm-4">
-    <div class="bs-sidebar">
-      <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-        <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un FbBot}}</a>
-        <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-        <?php
+    <div class="col-lg-2 col-md-3 col-sm-4">
+        <div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un fbbot}}</a>
+                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+                <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"  style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity .'"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
-?>
-     </ul>
+		    ?>
+           </ul>
+       </div>
    </div>
- </div>
 
- <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-  <legend>{{Mes Fbbot}}
-  </legend>
-
+   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+  <legend>{{Mes Fbbots}}</legend>
+  <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
   <div class="eqLogicThumbnailContainer">
-    <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-     <center>
-      <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-    </center>
-    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+      <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+        <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
+        <br>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
+    </div>
+      <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+      <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+    <br>
+    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
   </div>
-  <?php
-foreach ($eqLogics as $eqLogic) {
-	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-	echo "<center>";
-	echo '<img src="plugins/fbbot/doc/images/fbbot_icon.png" height="105" width="95" />';
-	echo "</center>";
-	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-	echo '</div>';
-}
+  </div>
+  <br /><br />
+  <legend><i class="fa fa-table"></i> {{Mes fbbots}}</legend>
+	<div class="eqLogicThumbnailContainer">
+    <?php
+	foreach ($eqLogics as $eqLogic) {
+		$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+		echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+		echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
+		echo "<br>";
+		echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
+		echo '</div>';
+	}
 ?>
 </div>
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-  <form class="form-horizontal">
-    <fieldset>
-      <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-      <div class="form-group">
-        <label class="col-sm-2 control-label">{{Nom de l'équipement}}</label>
-        <div class="col-sm-3">
-          <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-          <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}"/>
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" >Objet parent</label>
-        <div class="col-sm-3">
-            <select class="eqLogicAttr form-control" data-l1key="object_id">
-                 <option value="">Aucun</option>
-                 <?php
+	<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+  </ul>
+  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+      <br/>
+    <form class="form-horizontal">
+        <fieldset>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{Nom de l'équipement fbbot}}</label>
+                <div class="col-sm-3">
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement template}}"/>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                <div class="col-sm-3">
+                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                        <option value="">{{Aucun}}</option>
+                        <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-            </select>
-        </div>
-     </div>
-      <div class="form-group">
-        <label class="col-sm-2 control-label" >{{Activer}}</label>
-        <div class="col-sm-9">
-         <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-         <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
-       </div>
-     </div>
+                   </select>
+               </div>
+           </div>
+	<div class="form-group">
+		<label class="col-sm-3 control-label"></label>
+		<div class="col-sm-9">
+			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+		</div>
+	</div>
      <div class="form-group">
-     <label class="col-sm-2 control-label">{{Page ID}}</label>
+     <label class="col-sm-3 control-label">{{Page ID}}</label>
       <div class="col-sm-3">
         <input type="text" class="eqLogicAttr form-control" data-l1key="logicalId" placeholder="{{Page ID}}"/>
       </div>
     </div>
     <div class="form-group">
-      <label class="col-lg-2 control-label">{{URL de retour}}</label>
+      <label class="col-sm-3 control-label">{{URL de retour}}</label>
       <div class="col-lg-9">
         <span><?php echo network::getNetworkAccess('external') . '/plugins/fbbot/core/php/jeeFb.php';?></span>
   </div>
     </div>
     <div class="form-group">
-      <label class="col-lg-2 control-label">{{Verify Token}}</label>
+      <label class="col-sm-3 control-label">{{Verify Token}}</label>
       <div class="col-lg-9">
         <span><?php echo jeedom::getApiKey('fbbot');?></span>
       </div>
     </div>
     <div class="form-group">
-      <label class="col-lg-2 control-label">{{App secret}}</label>
+      <label class="col-sm-3 control-label">{{App secret}}</label>
       <div class="col-lg-9">
         <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="app_secret" placeholder="{{App secret}}"/>
       </div>
     </div>
     <div class="form-group">
-      <label class="col-lg-2 control-label">{{Facebook API Graph Access Token}}</label>
+      <label class="col-sm-3 control-label">{{Facebook API Graph Access Token}}</label>
       <div class="col-lg-9">
         <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="access_token" placeholder="{{Access token}}"/>
       </div>
     </div>
 	<div class="form-group">
-      <label class="col-lg-2 control-label">{{Créer les nouveaux contacts}}</label>
+      <label class="col-sm-3 control-label">{{Créer les nouveaux contacts}}</label>
       <div class="col-lg-9">
         <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="isAccepting" checked/>{{Activer}}</label>
       </div>
     </div>    
   </fieldset>
 </form>
-<p></p>
-<legend>{{Commandes}}</legend>
-<a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a><br/><br/>
+</div>
+      <div role="tabpanel" class="tab-pane" id="commandtab">
+<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Commandes}}</a><br/><br/>
 <table id="table_cmd" class="table table-bordered table-condensed">
-  <thead>
-    <tr>
-      <th>{{Nom}}</th><th>{{Facebook User ID}}</th><th>{{Jeedom username}}</th><th>{{Autorisations}}</th><th>{{Options}}</th>
-    </tr>
-  </thead>
-  <tbody>
-  </tbody>
+    <thead>
+        <tr>
+          <th>{{Nom}}</th><th>{{Facebook User ID}}</th><th>{{Jeedom username}}</th><th>{{Autorisations}}</th><th>{{Options}}</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
 </table>
-
-<form class="form-horizontal">
-  <fieldset>
-    <div class="form-actions">
-      <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-      <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-    </div>
-  </fieldset>
-</form>
+</div>
+</div>
 
 </div>
 </div>
